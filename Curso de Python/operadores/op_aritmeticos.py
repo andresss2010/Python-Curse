96ef9033ec60310036e129f86c6dbf67e7ec2a49
Andres <andresramosss453@gmail.com>
2026-04-20 16:33:51 -0600

    Add files via upload

---
+++ Curso de Python/operadores/op_aritmeticos.py
@@ -0,0 +1,28 @@
+#suma y resta ( + y - )
+suma = 5 + 3
+resta = 5 - 3
+
+#multiplicacion y division ( * y / )
+multiplicacion = 5 * 3
+division = 5 / 3
+
+#potenciacion ( ** )
+potencia = 5 ** 3
+
+#division baja ( // )
+division_baja = 5 // 3
+
+#resto o modulo ( % )
+resto = 5 % 3
+
+#type() nos devuelve el tipo de dato
+tipo_de_dato = type(resto)
+
+print("suma:", suma)
+print("resta:", resta)
+print("multiplicacion:", multiplicacion)
+print("division:", division)
+print("potencia:", potencia)
+print("division baja:", division_baja)
+print("resto:", resto)
+print("tipo de dato:", tipo_de_dato)
